473fe41455d24dad927769cc3b16102b452e4465
gu-th <1842987765@qq.com>
2024-07-17 14:32:12 +0800

    update

--- novel-script/config.js
+++ novel-script/config.js
@@ -2,38 +2,46 @@
  * @Author       : guth
  * @Date         : 2023-12-28 18:48:31
  * @LastEditors  : guth
- * @LastEditTime : 2023-12-28 19:08:19
- * @FilePath     : /my-tools/novel-script/config.js
+ * @LastEditTime : 2024-05-30 14:44:34
+ * @FilePath     : /tools/novel-script/config.js
  * @Description  : 爬小说的配置文件
  */
 // 编码
 const UTF8 = 'utf8'
 // 小说名
-const FILE_NAME = '朱雀书院'
+const FILE_NAME = '让你讨好女领导，你把人家娶了'
 // 小说路径
-const FILE_PATH = `../resource/小说/${FILE_NAME}.txt`
+const FILE_PATH = `./resource/${FILE_NAME}.txt`
 // 小说网址
-const URL_START = 'https://www.25zw.org/'
+const URL_START = 'https://69shu.net'
 // 第一章地址
-const FIRST_URL = 'https://www.25zw.org/147689/193366045.html'
+const FIRST_URL = 'https://69shu.net/6/6476/4074940.html'
 // 标题选择器
-const TITLE_SELECTOR = '.zhangjieming h1'
+const TITLE_SELECTOR = 'h2'
+// 正则过滤标题 1/2)
+// const REG_TITLE_RULE = /\（第([\d]+)页\）/g;
+const REG_TITLE_RULE = /\(\d(\/)\d\)/g
 // 内容选择器
-const CONTENT_SELECTOR = '#content > :not(:last-child)'
-// const CONTENT_SELECTOR = '#htmlContent p'
+// const CONTENT_SELECTOR = '.text > :not(:nth-last-child(-n+3))'
+// const CONTENT_SELECTOR = '.word_read > script:not(:nth-last-child(-n+3))'
+const CONTENT_SELECTOR = '#novelcontent.novelcontent'
 // 下一章选择器
-const NEXT_URL_SELECTOR = '.bottem a:nth-of-type(4)'
+const NEXT_URL_SELECTOR = '#next'
+// const NEXT_URL_SELECTOR = '.bottem a:nth-of-type(4)'
 // 结束标志
-const END_URL = '/book/147689.html'
+const END_URL = 'https://69shu.net/6/6476/4076396.html'
 // 是否需要转换编码
-const IS_TRANSFORM = false
+const IS_TRANSFORM = true
 // 请求axios 配置
-const AXIOS_CONFIG =  {
+const AXIOS_CONFIG = {
   responseType: IS_TRANSFORM ? 'arraybuffer' : UTF8, // 表示服务器响应的数据类型，设置为 buffer 类型
   headers: {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
-  }
+    'User-Agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+  },
 }
+// 需要删除的字符串集合
+const DELETE_STR_ARR = []
 
 module.exports = {
   UTF8,
@@ -41,9 +49,11 @@ module.exports = {
   URL_START,
   FIRST_URL,
   TITLE_SELECTOR,
+  REG_TITLE_RULE,
   CONTENT_SELECTOR,
   NEXT_URL_SELECTOR,
   END_URL,
   AXIOS_CONFIG,
-  IS_TRANSFORM
+  IS_TRANSFORM,
+  DELETE_STR_ARR,
 }
--- novel-script/t.js
+++ novel-script/t.js
@@ -2,8 +2,8 @@
  * @Author       : guth
  * @Date         : 2023-12-28 18:48:31
  * @LastEditors  : guth
- * @LastEditTime : 2023-12-28 19:02:57
- * @FilePath     : /my-tools/novel-script/t.js
+ * @LastEditTime : 2024-05-30 14:34:06
+ * @FilePath     : /tools/novel-script/t.js
  * @Description  : 爬个小说的简单脚本，软件上的广告太烦了
  */
 const axios = require('axios')
@@ -12,66 +12,120 @@ const fs = require('fs')
 const iconv = require('iconv-lite')
 const config = require('./config')
 
+const qsbs = require('./qsbs')
+
 const {
   UTF8,
   URL_START,
   FIRST_URL,
   TITLE_SELECTOR,
+  REG_TITLE_RULE,
   CONTENT_SELECTOR,
   NEXT_URL_SELECTOR,
   END_URL,
   AXIOS_CONFIG,
   IS_TRANSFORM,
-  FILE_PATH
+  FILE_PATH,
+  DELETE_STR_ARR,
 } = config
 
 const titleSet = new Set()
 
 console.log('config: ', config)
+console.log('==================================================== start ====================================================')
 
 requestApi(FIRST_URL)
 
 function requestApi(url) {
-  axios.get(url, AXIOS_CONFIG).then(res => {
-    if (res.status === 200) {
-      let htmlStr = res.data
-      IS_TRANSFORM && (htmlStr = iconv.decode(res.data, 'gbk'))
-      handleHtml(htmlStr)
-    }
-  }).catch(err => {
-    console.error('request err', err)
-    requestApi(url)
-  })
+  axios
+    .get(url, AXIOS_CONFIG)
+    .then((res) => {
+      if (res.status === 200) {
+        let htmlStr = res.data
+        IS_TRANSFORM && (htmlStr = iconv.decode(res.data, 'gbk'))
+        handleHtml(htmlStr, url)
+      }
+    })
+    .catch((err) => {
+      console.error(err)
+      console.error('request err')
+      requestApi(url)
+    })
 }
 
-function handleHtml(html) {
+function handleHtml(html, curUrl) {
   const $ = cheerio.load(html)
   writeContent($)
   const nextUrl = getNextUrl($)
-  if (nextUrl !== END_URL) {
+  console.log('curUrl:', curUrl)
+  if (curUrl !== END_URL) {
     requestApi(URL_START + nextUrl)
   } else {
     console.log('finished')
   }
 }
 
+/**
+ * @param {cheerio.CheerioAPI} $
+ */
 function writeContent($) {
-  const title = $(TITLE_SELECTOR).text()
-  const content = $(CONTENT_SELECTOR).text()
-  let str = ''
-  if (titleSet.has(title)) {
-    str = content + '\n'
-  } else {
-    titleSet.add(title)
-    str = title + '\n' + content + '\n'
+  const orgin_title = $(TITLE_SELECTOR).text() + '\n'
+  // const match = new RegExp(REG_TITLE_RULE).exec(orgin_title)
+  const match = orgin_title.match(REG_TITLE_RULE)
+  let title = orgin_title
+  if (match) {
+    title = orgin_title.replace(match[0], '')
   }
-  fs.appendFileSync(FILE_PATH, str, UTF8)
-  console.log(`write end ${title}`)
+  // #region 内容分段
+  let content = ''
+  $(CONTENT_SELECTOR).each((i, el) => {
+    if (URL_START === 'https://www.xywx.cc') {
+      const scriptStr = Object.values(el.children)[0].data
+      const pattern = /\'([^)]+)\'/g; // 匹配 '' 中间的内容
+      const match = scriptStr.match(pattern)
+      if (match) {
+        content += qsbs.bb(match[0]).replaceAll(/\<\/?p\>/g, '') + '\n'
+      }
+    } else {
+      // console.log('el', el.text());
+      content += $(el).text() + '\n'
+    }
+  })
+  // #endregion
+
+  // #region 需要删除的内容
+  if (DELETE_STR_ARR.length > 0) {
+    DELETE_STR_ARR.forEach((item) => {
+      content = content.replaceAll(item, '')
+    })
+  }
+  content = content.replaceAll('    ', '\n')
+  // #endregion
+
+  let writeTxt = content
+  if (!titleSet.has(title)) {
+    writeTxt = title + content
+    titleSet.add(title)
+  }
+  fs.appendFileSync(FILE_PATH, writeTxt, UTF8)
+  console.log(`write end ${orgin_title}`)
 }
 
+/**
+ * @param {cheerio.CheerioAPI} $
+ */
 function getNextUrl($) {
-  const nextUrl = $(NEXT_URL_SELECTOR)['0'].attribs.href
+  let nextUrl = ''
+
+  if (URL_START === 'https://69shu.net') {
+    const scripts =  $('script').text()
+    const arr = scripts.split(';')
+    const scriptStr = arr.filter(item => item.includes('next_page'))[0]
+    const pattern = /\"([^)]+)\"/g; // 匹配 '' 中间的内容
+    nextUrl = pattern.exec(scriptStr)[1]
+  } else {
+    nextUrl = $(NEXT_URL_SELECTOR)['0'].attribs.href
+  }
   console.log('next url: ', nextUrl)
   return nextUrl
 }
-
